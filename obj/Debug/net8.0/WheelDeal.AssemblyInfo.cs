863a157f04e61d7d1e597f868eafc3d903f2c25b
foksakirill <foksakirill@gmail.com>
2024-12-10 03:55:21 +0200

    finally..

--- obj/Debug/net8.0/WheelDeal.AssemblyInfo.cs
+++ obj/Debug/net8.0/WheelDeal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WheelDeal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5fcd5628bcceec64abfc1608d7d14c2cab9c221")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852cab4f4b75405526f6a436e67abe5b9a1e1162")]
 [assembly: System.Reflection.AssemblyProductAttribute("WheelDeal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WheelDeal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
